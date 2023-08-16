4c9e2362f558d8dd10320dd3dfd2698c7caa7c8c
q5089877 <q5089877@gmail.com>
2023-08-16 15:14:13 +0800

    ADS通訊完成

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_clamp_head = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Write";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_clamp_head
+            // 
+            this.btn_clamp_head.Location = new System.Drawing.Point(128, 12);
+            this.btn_clamp_head.Name = "btn_clamp_head";
+            this.btn_clamp_head.Size = new System.Drawing.Size(107, 56);
+            this.btn_clamp_head.TabIndex = 3;
+            this.btn_clamp_head.Text = "夾住筆頭";
+            this.btn_clamp_head.UseVisualStyleBackColor = true;
+            this.btn_clamp_head.Click += new System.EventHandler(this.btn_clamp_head_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 664);
+            this.Controls.Add(this.btn_clamp_head);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_clamp_head;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwinCAT.Ads;  //Step1引用ADS    在References下新增TwinCAT.Ads(如有安裝TwinCAT，DLL位置在C:\TwinCAT\AdsApi\.NET\v4.0.30319\TwinCAT.Ads.dll)
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //IO數量宣告     
+        private const int doNum = 2;
+
+        //Step2 ADS Client宣告
+        private TcAdsClient adsClient = new TcAdsClient();
+
+        //主動回報
+        private int[] hConnect;
+        private AdsStream dataStream;
+        private BinaryReader binRead;
+
+        //Step3 IO Handl宣告
+        private int hDigitalInputs;
+        private int hDigitalOutputs;
+
+        //Step4 C#端IO實際變數宣告
+        [MarshalAs(UnmanagedType.I1)]
+        private bool DigitalInput1;
+        private bool DigitalInput2;
+        private bool DigitalInput3;
+        private bool DigitalInput4;
+        private bool DigitalInput5;
+        private bool DigitalInput6;
+        private bool DigitalInput7;
+        private bool DigitalInput8;
+        private bool[,] DigitalOutputs = new bool[doNum, 16];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step5 填入要連結的ADS位置
+                adsClient.Connect(853);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("853");
+                MessageBox.Show(err.Message);
+            }
+
+            try
+            {
+                //Step6 取得IO Handle數值
+                // hDigitalInputs = adsClient.CreateVariableHandle("GVL.DigitalInputs");
+                hDigitalOutputs = adsClient.CreateVariableHandle("GVL.DigitalOutputs");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        //Read
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            //using (TcAdsClient client = new TcAdsClient())
+            //{
+            //    client.Connect(853); // Connect to local port 851 (PLC)              
+
+            //    try
+            //    {
+            //        DigitalInputs = (bool[,])adsClient.ReadAny(hDigitalInputs, typeof(bool[,]), new int[] { diNum, 16 });
+            //        for (int i = 0; i < 16; i++)
+            //        {
+            //            MessageBox.Show(DigitalInputs[0, i].ToString());
+            //        }
+            //    }
+            //    catch (Exception err)
+            //    {
+            //        MessageBox.Show(err.Message);
+            //    }
+            //}
+        }
+
+        //Write
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step8 IO資料寫入
+                for (int i = 0; i < 16; i++)
+                {
+                    DigitalOutputs[0, i] = true;
+                }
+
+                // 寫入陣列                
+                adsClient.WriteAny(hDigitalOutputs, DigitalOutputs);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step5 填入要連結的ADS位置
+                adsClient.Connect(853);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("853");
+                MessageBox.Show(err.Message);
+            }
+
+            try
+            {
+                //Step6 取得IO Handle數值
+                // hDigitalInputs = adsClient.CreateVariableHandle("GVL.DigitalInputs");
+                hDigitalOutputs = adsClient.CreateVariableHandle("GVL.DigitalOutputs");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+
+
+
+
+
+
+            try
+            {
+                //   adsClient.Connect(853);
+
+                dataStream = new AdsStream(31);
+                //Encoding is set to ASCII, to read strings
+                binRead = new BinaryReader(dataStream);
+                hConnect = new int[8];
+
+                //增加ADS主動回報
+                hConnect[0] = adsClient.AddDeviceNotification("GVL.DigitalInput1", dataStream, 0, 1, AdsTransMode.OnChange, 100, 0, DigitalInput1);
+                hConnect[1] = adsClient.AddDeviceNotification("GVL.DigitalInput2", dataStream, 1, 2, AdsTransMode.OnChange, 100, 0, DigitalInput2);
+                hConnect[2] = adsClient.AddDeviceNotification("GVL.DigitalInput3", dataStream, 2, 3, AdsTransMode.OnChange, 100, 0, DigitalInput3);
+                hConnect[3] = adsClient.AddDeviceNotification("GVL.DigitalInput4", dataStream, 3, 4, AdsTransMode.OnChange, 100, 0, DigitalInput4);
+                hConnect[4] = adsClient.AddDeviceNotification("GVL.DigitalInput5", dataStream, 4, 5, AdsTransMode.OnChange, 100, 0, DigitalInput5);
+                hConnect[5] = adsClient.AddDeviceNotification("GVL.DigitalInput6", dataStream, 5, 6, AdsTransMode.OnChange, 100, 0, DigitalInput6);
+                hConnect[6] = adsClient.AddDeviceNotification("GVL.DigitalInput7", dataStream, 6, 7, AdsTransMode.OnChange, 100, 0, DigitalInput7);
+                hConnect[7] = adsClient.AddDeviceNotification("GVL.DigitalInput8", dataStream, 7, 8, AdsTransMode.OnChange, 100, 0, DigitalInput8);
+                adsClient.AdsNotification += new AdsNotificationEventHandler(OnNotification);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("增加ADS主動回報");
+                // MessageBox.Show(err.Message);
+            }
+        }
+
+        private void OnNotification(object sender, AdsNotificationEventArgs e)
+        {
+            try
+            {
+                if (e.NotificationHandle == hConnect[0])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input1 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input1 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[1])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input2 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input2 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[2])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input3 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input3 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[3])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input4 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input4 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[4])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input5 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input5 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[5])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input6 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input6 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[6])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input7 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input7 is false");
+                    }
+                }
+                else if (e.NotificationHandle == hConnect[7])
+                {
+                    if (binRead.ReadBoolean())
+                    {
+                        MessageBox.Show("input8 is true");
+                    }
+                    else
+                    {
+                        MessageBox.Show("input8 is false");
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    adsClient.DeleteDeviceNotification(hConnect[i]);
+                }
+            }
+            catch (Exception err)
+            {
+                // MessageBox.Show(err.Message);
+            }
+            adsClient.Dispose();
+        }
+
+        private void btn_clamp_head_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
